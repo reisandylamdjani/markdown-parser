f98f5220ed80a81c06d8c31c5d55875322b75e6e
reisandylamdjani <lamdjani@gmail.com>
2022-04-14 11:30:07 -0700

    noLinkTest

--- MarkdownParse.java
+++ MarkdownParse.java
@@ -17,13 +17,8 @@ public class MarkdownParse {
                 int closeBracket = markdown.indexOf("]", openBracket);
                 int openParen = markdown.indexOf("(", closeBracket);
                 int closeParen = markdown.indexOf(")", openParen);
-            
-                if(openBracket == -1 || closeBracket == -1 || openParen == -1
-                    || closeParen == -1)
-                    {
-                        currentIndex += 1;
-                        continue;
-                    }
+    
+                
                 toReturn.add(markdown.substring(openParen + 1, closeParen));
                 currentIndex = closeParen + 1;
             }
