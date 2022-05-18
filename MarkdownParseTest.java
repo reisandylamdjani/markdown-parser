82d584735dfb52765de2b7ba5ecfa46f60f3670d
reisandylamdjani <lamdjani@gmail.com>
2022-05-17 23:57:20 -0700

    hw

--- MarkdownParseTest.java
+++ MarkdownParseTest.java
@@ -85,11 +85,5 @@ public class MarkdownParseTest {
         ArrayList<String> al = MarkdownParse.getLinks(content);
         assertEquals(List.of("https://something.com", "some-thing.html"), al);
     }
-}
 
-/* 
-scp -r . cs15lsp22aon@ieng6.ucsd.edu:~/markdown-parse;
-"cd markdown-parse;
-/software/CSE/oracle-java-17/jdk-17.0.1/bin/javac MarkdownParse.java;
-/software/CSE/oracle-java-17/jdk-17.0.1/bin/javac -cp .:lib/junit-4.13.2.jar:lib/hamcrest-core-1.3.jar MarkdownParseTest.java; /software/CSE/oracle-java-17/jdk-17.0.1/bin/java -cp .:lib/junit-4.13.2.jar:lib/hamcrest-core-1.3.jar org.junit.runner.JUnitCore MarkdownParseTest"
-*/
+}
